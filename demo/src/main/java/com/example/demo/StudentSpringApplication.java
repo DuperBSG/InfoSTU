b0151106315b00fec487ebb803ec67e1c27e93f4
DuperBSG <Bowei2017818@gmail.com>
2024-11-20 22:46:02 -0600

    Api update
    Now ready to connect to api

--- demo/src/main/java/com/example/demo/StudentSpringApplication.java
+++ demo/src/main/java/com/example/demo/StudentSpringApplication.java
@@ -1,14 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -16,11 +10,6 @@ public class StudentSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentSpringApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(new Student(1L, "Tim Duncan", "timmy@gmail.com", LocalDate.of(2020, Month.FEBRUARY, 5), 21));
 	}
 
 }
