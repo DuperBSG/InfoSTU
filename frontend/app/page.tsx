0ae1908f95f2088bcae9aecde2473895f7448dbe
Tian <xtyao2015@gmail.com>
2024-11-23 15:06:28 -0500

    add theme selection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import {
   Table,
   TableBody,
@@ -10,25 +11,28 @@ import {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-3xl">Student Dashboard</h1>
-        <Table>
-          <TableCaption>A list of students broski dowski</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Name</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>Date of Birth</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20">
+      <h1 className="text-3xl">Student Dashboard</h1>
+      <div className="flex w-full justify-end">
+        <ThemeToggle />
+      </div>
+      <Table>
+        <TableCaption>A list of students broski dowski</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Date of Birth</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">LeBron James</TableCell>
+            <TableCell>lebron@gmail.com</TableCell>
+            <TableCell>1990/05/12</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
