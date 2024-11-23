35a7057ae0f38620e3b9df04f7dce806c800d089
Tian <xtyao2015@gmail.com>
2024-11-23 14:51:04 -0500

    add table with hardcoded data and remove unused imports

--- demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -3,12 +3,9 @@ package com.example.demo.student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.time.LocalDate;
 import java.util.List;
-
 import static java.time.Month.JANUARY;
-
 
 @Configuration
 public class StudentConfig {
