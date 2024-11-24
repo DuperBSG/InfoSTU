03793ac05ad44ecfdd0e04ec72d93fd7b2c9aeee
DuperBSG <Bowei2017818@gmail.com>
2024-11-24 16:41:45 -0600

    aws connection

--- demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -14,7 +14,8 @@ public class StudentConfig {
         return args -> {
               Student timmy = new Student ( "Tim Duncan", "timmy@gmail.com", LocalDate.of(2000, JANUARY, 2));
               Student jordan = new Student (  "Michael Jordan", "jordan@gmail.com", LocalDate.of(1999, JANUARY, 2));
-              repository.saveAll(List.of(timmy, jordan));
+              Student abc = new Student (  "abc", "abc@gmail.com", LocalDate.of(1999, JANUARY, 2));
+              repository.saveAll(List.of(timmy, jordan, abc));
         };
     }
 }
