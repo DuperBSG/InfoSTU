c99e877415831abeb556ca20d478d88c8ec2bd82
Tian Yao <xtyao2015@gmail.com>
2024-11-24 18:45:41 -0500

    add aws keys in env

--- demo/src/main/java/com/example/demo/StudentSpringApplication.java
+++ demo/src/main/java/com/example/demo/StudentSpringApplication.java
@@ -3,12 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @RestController
 public class StudentSpringApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(StudentSpringApplication.class, args);
 	}
 
